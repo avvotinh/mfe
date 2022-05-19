001bb49a913f2f69f4dd51288e84b3210cb87da2
Le Van Hop <hop.le@axonactive.com>
2022-05-19 17:17:36 +0700

    add container header

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,12 +1,15 @@
 import React from "react";
+import { BrowserRouter } from "react-router-dom";
+import Header from "./components/Header";
 import MarketingApp from "./components/MarketingApp";
 
 export default () => {
   return (
-    <React.Fragment>
-      <h1>Container app</h1>
-      <hr />
-      <MarketingApp />
-    </React.Fragment>
+    <BrowserRouter>
+      <React.Fragment>
+        <Header />
+        <MarketingApp />
+      </React.Fragment>
+    </BrowserRouter>
   );
 };
